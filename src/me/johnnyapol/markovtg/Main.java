dd8a20d1109dda1e658b8b5febee23b184624a18
John C. Allwein <john.allwein57@gmail.com>
2016-05-28 16:20:15 -0400

    Add copyright headers

--- src/me/johnnyapol/markovtg/Main.java
+++ src/me/johnnyapol/markovtg/Main.java
@@ -1,7 +1,26 @@
+/* This file is part of markov-tg.
+
+    markov-tg is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    markov-tg is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with markov-tg.  If not, see <http://www.gnu.org/licenses/>. */
 package me.johnnyapol.markovtg;
 
 import java.io.Console;
 
+/** 
+ * Simple main example class - you can ignore for now
+ * @author John C. Allwein <johnnyapol>
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
--- src/me/johnnyapol/markovtg/SentenceGenerator.java
+++ src/me/johnnyapol/markovtg/SentenceGenerator.java
@@ -1,3 +1,17 @@
+/* This file is part of markov-tg.
+
+    markov-tg is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    markov-tg is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with markov-tg.  If not, see <http://www.gnu.org/licenses/>. */
 package me.johnnyapol.markovtg;
 
 import me.johnnyapol.markovtg.probability.ProbabilityEngine;
